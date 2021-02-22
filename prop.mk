6b6c0cbe106d3d586fcb4d27f7d5c0f9677916a5
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-22 20:51:35 +0100

    vince: Resurrect DPM features
    adding back Qualcomm Data Path Manager functionalities:
    * re-enable (FD) fast dormancy
    * explicitly enable dpm hal
    * enable DPM MwqemService
    
    Change-Id: I7f0007cbe5955e9318291582f27b58116f9c69fc
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- prop.mk
+++ prop.mk
@@ -101,7 +101,10 @@ vendor.video.disable.ubwc=1
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.dpm.feature=0
+persist.vendor.dpm.feature=1 \
+persist.vendor.dpmhalservice.enable=1 \
+persist.vendor.dpm.loglevel=0 \
+persist.vendor.mwqem.enable=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
