63ad1824b0e368aa5d9b7f0b2e604823f867ff6b
root2raj <ruturaj1999@gmail.com>
2021-03-01 22:10:02 +0000

    vince: Update to coral april fp

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -39,4 +39,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys
