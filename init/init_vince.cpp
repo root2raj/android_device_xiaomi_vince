92a166f1ee73395907c70f5b602682bbc59551cc
root2raj <ruturaj1999@gmail.com>
2021-04-22 03:38:50 +0530

    vince: Set product model correctly

--- init/init_vince.cpp
+++ init/init_vince.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2019-2020, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -25,25 +26,104 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <fcntl.h>
-#include <stdlib.h>
+#include <string.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "log/log.h"
 #include "property_service.h"
 #include "vendor_init.h"
 
-int property_set(const char *key, const char *value) {
-    return __system_property_set(key, value);
+using android::base::GetProperty;
+using std::string;
+
+std::vector<std::string> ro_props_default_source_order = {
+        "", "odm.", "product.", "system.", "system_ext.", "vendor.",
+};
+
+char const* heapstartsize;
+char const* heapgrowthlimit;
+char const* heapsize;
+char const* heapminfree;
+char const* heapmaxfree;
+char const* heaptargetutilization;
+
+void property_override(char const prop[], char const value[], bool add = true) {
+    auto pi = (prop_info*)__system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_dalvik_props() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+void set_model_props() {
+    const auto set_ro_product_prop = [](const std::string& source, const std::string& prop,
+                                        const std::string& value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    string region = GetProperty("ro.boot.hwc", "");
+    string model;
+    if (region == "India") {
+        model = "Redmi Note 5";
+
+    } else {
+        model = "Redmi 5 Plus";
+    }
+
+    for (const auto& source : ro_props_default_source_order) {
+        set_ro_product_prop(source, "model", model);
+    }
 }
 
 void vendor_load_properties() {
-  property_set("dalvik.vm.heapstartsize", "8m");
-  property_set("dalvik.vm.heapgrowthlimit", "192m");
-  property_set("dalvik.vm.heapsize", "512m");
-  property_set("dalvik.vm.heaptargetutilization", "0.6");
-  property_set("dalvik.vm.heapminfree", "8m");
-  property_set("dalvik.vm.heapmaxfree", "16m");
+    set_dalvik_props();
+    set_model_props();
 }
