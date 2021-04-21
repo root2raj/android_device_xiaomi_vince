e7dd8dab165f5cb0fa8659b3dcbb05c11213e30a
root2raj <ruturaj1999@gmail.com>
2021-04-22 03:14:47 +0530

    Revert "vince: Copy prebuilt camera provider"
    This reverts commit 6bdbe6c9f6f3b782b7e2e5e2ba0e7a4e608e9ad2.

--- device.mk
+++ device.mk
@@ -150,6 +150,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
     libdng_sdk.vendor \
