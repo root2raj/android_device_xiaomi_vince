cb3b72383f5ec29981d33498cf1c97f391a19c0f
starlight5234 <starlight5234@protonmail.ch>
2021-04-22 02:35:05 +0530

    vince: Add EsePowerManager to vintf manifest
    E HidlServiceManagement: Service vendor.qti.esepowermanager@1.0::IEsePowerManager/default must be in VINTF manifest in order to register/get.
    E vendor.qti.esepowermanager@1.0-service: Could not register service vendor.qti.esepowermanager@1.0::IEsePowerManager/default (-2147483648).
    F vendor.qti.esepowermanager@1.0-service: Error while registering eSE power manager service: -2147483648

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ TARGET_NO_RPC := true
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vintf/vendor.qti.esepowermanager@1.0-service.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
