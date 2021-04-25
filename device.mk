a4e56cd4797e850d4878a95335afdff1e898e8ea
root2raj <ruturaj1999@gmail.com>
2021-04-25 09:18:30 +0000

    vince: Build Snap

--- device.mk
+++ device.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    libdng_sdk.vendor
+    libdng_sdk.vendor \
+    Snap
 
 # Consumerir
 PRODUCT_PACKAGES += \
